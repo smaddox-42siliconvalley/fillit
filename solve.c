9833a5d35ae609a21e18e4ef08a4196ed9f386a1
Samuel Maddox <smaddox@e1z3r3p4.42.us.org>
2019-06-05 14:07:27 -0700

    fixed solver

--- solve.c
+++ solve.c
@@ -2,48 +2,47 @@
 
 int		solver(column_o *master_co, piece *arr)
 {
-	t_cell		*choice;
-	column_o	*current;
+	t_cell *choice;
+	column_o *current;
 
 	current = master_co->next;
-	if (current->union_type == 1)
-		return (1);
-	if (current->list_header.D == &(current->list_header))
-		return (0);
-	while (current->union_type == 0)
+	if (current == master_co)
+		return(1);
+	choice = current->list_header.D;
+	choice_helper(choice, arr);
+	cover_choice(choice);
+	while (choice->a == 0)
 	{
-		choice = current->list_header.D;
-		while (choice->a == 0)
+		if((solver(master_co, arr)) == 1)
 		{
-			cover_choice(choice);
-			if ((solver(master_co, arr)))
-			{
-				uncover_choice(choice);
-				choice_helper(choice, arr);
-				return (1);
-			}
-			uncover_choice(choice);
-			choice = choice->D;
+			return(1);
 		}
-		current = current->next;
+		uncover_choice(choice);
+		choice = choice->D;
+		choice_helper(choice, arr);
+		cover_choice(choice);
 	}
-	return (0);
+	uncover_choice(choice);
+	return(0);
 }
 
 void	choice_helper(t_cell *choice, piece *arr)
 {
 	char	id;
-	int		i;
+	int 	i;
 	int		j;
 
 	j = -1;
 	i = -1;
 	id = choice->C->colname.id;
 	choice = choice->R;
-	while (arr[++i].id != id){;}
-	while (++j < 4)
+	while(arr[++i].id != id){;}
+	while(++j<4)
 	{
 		arr[i].blocks[j].x = choice->C->colname.row_num;
 		choice = choice->R;
 	}
 }
+
+
+		
